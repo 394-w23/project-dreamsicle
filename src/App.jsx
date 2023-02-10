8c7cdcbc5a9232dc8bd4357cec7f0dbd991394db
Adam Stewart <adamjoesph111@gmail.com>
2023-02-10 16:55:18 -0600

    working to get routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,62 @@
 import { useState } from 'react';
 import logo from './logo.svg';
+import { useDbData } from './utils/firebase';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RestarauntPage from './components/RestarauntPage';
 import './App.css';
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [data, error] = useDbData("/");
+
+  if (error) return <h1>Error loading data: {error.toString()}</h1>;
+  if (data === undefined) return <h1>Loading data...</h1>;
+  if (!data) return <h1>No data found</h1>;
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test hot module replacement (HMR).
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <BrowserRouter>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello Vite + React!</p>
+          <p>
+            <button onClick={() => setCount(count => count + 1)}>
+              count is: {count}
+            </button>
+          </p>
+          <p>
+            Edit <code>App.jsx</code> and save to test hot module replacement (HMR).
+          </p>
+          <p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            {' | '}
+            <a
+              className="App-link"
+              href="https://vitejs.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Vite Docs
+            </a>
+          </p>
+        </header>
+        <div>
+        <Routes>
+            <Route
+              path="/:restaraunt_id"
+              element={<RestarauntPage
+              />}
+            />
+            </Routes>
+            </div>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/components/RestarauntPage.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { useParams } from "react-router-dom";
+import logo from '../logo.svg';
+
+const RestarauntPage = () => {
+    const { id } = useParams();
+
+    const [count, setCount] = useState(0);
+
+    return (<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+            <button onClick={() => setCount(count => count + 1)}>
+                count is: {count}
+            </button>
+        </p>
+        <p>
+            Edit <code>App.jsx</code> and save to test hot module replacement (HMR).
+        </p>
+        <p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+            {' | '}
+            <a
+                className="App-link"
+                href="https://vitejs.dev/guide/features.html"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Vite Docs
+            </a>
+        </p>
+    </header>);
+}
+
+export default RestarauntPage;
