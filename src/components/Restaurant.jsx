7d1153a56c0b8840b5899c6fd58a6e23c148fe21
murirop <panashemuriro2023@u.northwestern.edu>
2023-02-13 17:50:54 -0600

    Restaurant card from mantine

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { useParams, Link } from "react-router-dom";
 import logo from '../logo.svg';
 import './Restaurant.css';
+import { Card, Image, Text, Badge, Button, Group } from '@mantine/core';
+
 
 const Restaurant = ({ restaurant, setRestaurantID, setRestaurant }) => {
     const { id } = useParams();
@@ -12,13 +14,52 @@ const Restaurant = ({ restaurant, setRestaurantID, setRestaurant }) => {
 
     return (
         <div className='restaurant'>
-            <Link to={`/${restaurant.id}`}>
-                <img className='photo' src={`${restaurant.profile.photo}`}></img>
-                <div className='name'>{restaurant.profile.name}</div>
-                {/* <div>{restaurant.profile.description}</div> */}
-                <div className='notice'>Notice Time: 24 hrs</div>
-                <div className='person-range'>{restaurant.profile.lower_order_bound}-{restaurant.profile.upper_order_bound}</div>
-                <div onClick={onClick}>Click Me</div>
+            <Link to={`/${restaurant.id}`} style={{textDecoration: 'none'}}>
+
+            <Card shadow="sm" p="lg" radius="md" withBorder>
+      <Card.Section>
+        <Image
+          src={restaurant.profile.photo}
+          height={160}
+          alt="Restaurant image"
+        />
+      </Card.Section>
+
+      <Group position="apart" mt="md" mb="xs">
+      <Text weight={500}>{restaurant.profile.name}</Text>
+    <Text weight={500}>$$</Text>
+        
+      </Group>
+
+
+      <Group position="apart" mt="md" mb="xs">
+      <Text>Advance notice: {restaurant.profile.advance_notice} </Text>
+      <Text>{restaurant.profile.lower_order_bound}-{restaurant.profile.upper_order_bound} people</Text>
+
+      </Group>
+
+
+
+
+    </Card>
+
+
+{/*                
+                <div className="">
+                    <img className='photo' src={`${restaurant.profile.photo}`}></img>
+                </div>
+                <div className="details">
+
+
+                    <div className='name'>{restaurant.profile.name}</div>
+                 
+                    <div className='notice'>Notice Time: 24 hrs</div>
+                    <div className='person-range'>{restaurant.profile.lower_order_bound}-{restaurant.profile.upper_order_bound}</div>
+                    <div onClick={onClick}>Click Me</div>
+
+                </div> */}
+
+                
             </Link>
         </div>);
 }
