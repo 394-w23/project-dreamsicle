a5c2163ebd3e7fba1d56eda9b8a743e71c09efab
jiajuyu <jiajuyu2024@u.northwestern.edu>
2023-02-21 13:09:08 -0600

    change filter style

--- src/components/FilterItem.jsx
+++ src/components/FilterItem.jsx
@@ -1,12 +1,19 @@
 import './FilterItem.css'
 import { RiTruckLine } from "@react-icons/all-files/ri/RiTruckLine"
+import { Badge } from '@mantine/core';
 
 const FilterItem = ({tag, setTagFilter}) => {
 
     return(
-        <div onClick={() => setTagFilter(tag)} className="filter-item"><RiTruckLine size={12} />
-        {tag}</div>
+    <div onClick={() => setTagFilter(tag)} className="filter-tag">
+      <Badge variant="gradient" gradient={{ from: 'indigo', to: 'cyan' }}>{tag}</Badge>
+    </div>
     );
+
+    // return(
+    //     <div onClick={() => setTagFilter(tag)} className="filter-item"><RiTruckLine size={12} />
+    //     {tag}</div>
+    // );
 }
 
 export default FilterItem
