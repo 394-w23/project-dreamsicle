088ce5e11ddc193de696d9e4e381bcb786c7b4d4
Adam Stewart <adamjoesph111@gmail.com>
2023-02-21 11:39:09 -0600

    setting a filter is now a reversisble action

--- src/components/FilterItem.jsx
+++ src/components/FilterItem.jsx
@@ -1,10 +1,11 @@
 import './FilterItem.css'
-
+import { RiTruckLine } from "@react-icons/all-files/ri/RiTruckLine"
 
 const FilterItem = ({tag, setFilter}) => {
 
     return(
-        <div onClick={() => setFilter(tag)}>{tag}</div>
+        <div onClick={() => setFilter(tag)} className="filter-item"><RiTruckLine size={12} />
+        {tag}</div>
     );
 }
 
--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -12,15 +12,22 @@ import FilterItem from './FilterItem';
 
 const RestaurantList = ({ restaurants }) => {
     const [filteredRestaurants, setFilteredRestaurants] = useState(restaurants);
+    const [currFilter, setcurrFilter] = useState("")
     const setFilter = (filter) => {
         let tempList = restaurants.filter(restaurant => restaurant.profile.tags.includes(filter))
-        setFilteredRestaurants(tempList)
+        if (filter === currFilter) {
+            setcurrFilter("")
+            setFilteredRestaurants(restaurants)
+        } else {
+            setcurrFilter(filter)
+            setFilteredRestaurants(tempList)
+        }
     }
 
     return (
         <div>
             <Header />
-            <div>{tags.map(tag => <FilterItem key={tag} tag={tag} setFilter={setFilter}/>)}</div>
+            <div className='tags'>{tags.map(tag => <FilterItem key={tag} tag={tag} setFilter={setFilter} />)}</div>
             <div className='restaurant-list'>
                 {filteredRestaurants.map(r => <Restaurant key={r.id} restaurant={r} />)}
             </div>
