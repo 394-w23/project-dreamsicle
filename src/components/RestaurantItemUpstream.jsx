f4e80859df0a661749bba75ac8e73b5387ae9c2f
finnwintz5 <102768648+finnwintz5@users.noreply.github.com>
2023-02-27 08:49:57 -0600

    upstream complete (protections to be implemented)

--- src/components/RestaurantItemUpstream.jsx
+++ src/components/RestaurantItemUpstream.jsx
@@ -3,37 +3,36 @@ import { useDbData, useDbUpdate, getDbStorage } from '../utils/firebase';
 import uuid from 'react-uuid';
 import { useEffect, useRef, useState } from 'react';
 import { useForm } from '@mantine/form';
+import { showNotification } from '@mantine/notifications';
+
 
 export const RestaurantItemUpstream = ({ user, setCurrDisplay }) => {
-    const [update, result] = useDbUpdate(`/restaurants/3/menu_sections}`);
-    const [menu_sections, error] = useDbData(`/restaurants/3/menu_sections}`);
+    const [updateMenuSections, result] = useDbUpdate(`/restaurants/3`);
+    const [menu_sections, error] = useDbData(`/restaurants/3`);
     const formRef = useRef(null); // to disable form submission on enter
     // LISTS
     const [addOns, setAddOns] = useState([])
     const [customizableCategories, setCustomizableCategories] = useState([])
     const [items, setItems] = useState([])
-    const [newMenuSections, setNewMenuSections] = useState(menu_sections)
+    const [newMenuSections, setNewMenuSections] = useState(menu_sections === undefined || menu_sections === null ? [] : menu_sections)
     // CURRENT TEXT
-    const [currentAddOn, setCurrentAddOn] = useState({name:"",price:0})
-    const [currentCustomizableCategory, setCurrentCustomizableCategory] = useState({name:"",desc:"",required_amount:""})
-    const [currentItem, setCurrentItem] = useState({name:"",price:"",servings:"",tags:"",ingredients:"",photo:""})
+    const [currentAddOn, setCurrentAddOn] = useState({ name: "", price: 0 })
+    const [currentCustomizableCategory, setCurrentCustomizableCategory] = useState({ name: "", desc: "", required_amount: 0 })
+    const [currentItem, setCurrentItem] = useState({ name: "", price: 0, servings: 0, tags: "", ingredients: "", photo: "" })
     const [currentNewMenuSections, setCurrentNewMenuSections] = useState("")
 
-    // let new_menu_sections = menu_sections
-    // let add_ons = []
-    // let customizable_categories = []
-    // let items = []
-    // console.log(currentAddOn)
+    // console.log(newMenuSections)
+
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
             event.preventDefault();
         }
     };
 
-    console.log("currentAddOn: ", currentAddOn)
-    console.log("currentCustomizableCategory: ", currentCustomizableCategory)
-    console.log("currentItem: ", currentItem)
-    console.log("currentNewMenuSections: ", currentNewMenuSections)
+    // console.log("currentAddOn: ", currentAddOn)
+    // console.log("currentCustomizableCategory: ", currentCustomizableCategory)
+    // console.log("currentItem: ", currentItem)
+    // console.log("currentNewMenuSections: ", currentNewMenuSections)
 
     const form = useForm({
         initialValues: {
@@ -46,58 +45,92 @@ export const RestaurantItemUpstream = ({ user, setCurrDisplay }) => {
 
 
 
-    
+    const displayInfo = () => {
+        console.log("addOns", addOns)
+        console.log("customizableCategories", customizableCategories)
+        console.log("items", items)
+        console.log("newMenuSections", newMenuSections)
+    }
 
-    const createAddOn = (add_on_name, add_on_price) => {
+    const createAddOn = () => {
 
         let add_on = {
             id: uuid(),
-            name: add_on_name,
-            price: add_on_price,
+            name: currentAddOn.name,
+            price: currentAddOn.price,
         }
-        let newAddOns=addOns
+        let newAddOns = addOns
         newAddOns.push(add_on)
         setAddOns(newAddOns)
+        showNotification({
+            message: `Created ${add_on.name} add on`,
+            autoClose: 1500,
+        });
+
+        setCurrentAddOn({ name: "", price: 0 })
 
     }
 
-    const createCustomizableCategory = (add_ons, required_amount, name, desc) => {
+    const createCustomizableCategory = () => {
 
         let customizable_category = {
             id: uuid(),
-            name: name,
-            desc: desc,
-            'required-select-amount': required_amount,
-            "customizable-add-ons": add_ons,
+            name: currentCustomizableCategory.name,
+            desc: currentCustomizableCategory.desc,
+            'required-select-amount': currentCustomizableCategory.required_amount,
+            "customizable-add-ons": addOns,
         }
-        let newCustomizableCategories=customizableCategories
+        let newCustomizableCategories = customizableCategories
         newCustomizableCategories.push(customizable_category)
         setCustomizableCategories(newCustomizableCategories)
+        showNotification({
+            message: `Created ${customizable_category.name} customizableCategory`,
+            autoClose: 1500,
+        });
+        setCurrentCustomizableCategory({ name: "", desc: "", required_amount: 0 })
+        setAddOns([])
     }
 
-    const createItem = (name, price, servings, tags, ingredients, photo) => {
+    const createItem = () => {
         let item = {
-            name: name,
-            price: price,
-            servings: servings,
-            tags: tags,
-            ingredients: ingredients,
-            photo: photo,
+            name: currentItem.name,
+            price: currentItem.price,
+            servings: currentItem.servings,
+            tags: currentItem.tags,
+            ingredients: currentItem.ingredients,
+            photo: currentItem.photo,
+            "customizable-categories": customizableCategories
         }
-        let new_items=items
+        let new_items = items
         new_items.push(item)
         setItems(new_items)
+        showNotification({
+            message: `Created ${item.name} item`,
+            autoClose: 1500,
+        });
+        setCurrentItem({ name: "", price: 0, servings: 0, tags: "", ingredients: "", photo: "" })
+        setCustomizableCategories([])
     }
 
-    const createMenuSection = (name, items) => {
+    const createMenuSection = () => {
         let menu_section = {
             id: uuid(),
-            name: name,
-            items: items,
+            name: currentNewMenuSections,
+            items: items
         }
-        let new_menu_sections=newMenuSections
+        let new_menu_sections = newMenuSections
         new_menu_sections.push(menu_section)
         setNewMenuSections(new_menu_sections)
+
+        
+
+        setCurrentNewMenuSections("")
+        showNotification({
+            message: `Created ${menu_section.name} section`,
+            autoClose: 1500,
+        });
+        setItems([])
+        updateMenuSections({menu_sections:new_menu_sections})
     }
 
     const submitForm = (e) => {
@@ -109,81 +142,82 @@ export const RestaurantItemUpstream = ({ user, setCurrDisplay }) => {
 
         <>
             <form data-cy="create-hobby-form" onSubmit={submitForm} ref={formRef} onKeyDown={handleKeyDown}>
+                <Button onClick={displayInfo}>CONSOLE.LOG</Button>
                 <div className="add-on">
                     <Text>ADD ON</Text>
                     <TextInput
                         label="Name"
                         value={currentAddOn.name}
-                        onChange={(event)=>setCurrentAddOn({name:event.currentTarget.value,price:currentAddOn.price})}
+                        onChange={(event) => setCurrentAddOn({ ...currentAddOn, name: event.currentTarget.value })}
                     />
                     <NumberInput
                         label="Price"
                         value={currentAddOn.price}
-                        onChange={(val)=>setCurrentAddOn({name:currentAddOn.name,price:val})}
+                        onChange={(val) => setCurrentAddOn({ ...currentAddOn, price: val })}
                     />
-                    <Button>Add to add_on list</Button>
+                    <Button onClick={createAddOn}>Add to add_on list</Button>
                 </div>
                 <div className="customizable-category">
                     <Text>CUSTOMIZABLE CATEGORY</Text>
                     <TextInput
                         label="Name"
                         value={currentCustomizableCategory.name}
-                        onChange={(event)=>setCurrentCustomizableCategory({...currentCustomizableCategory, name:event.currentTarget.value})}
+                        onChange={(event) => setCurrentCustomizableCategory({ ...currentCustomizableCategory, name: event.currentTarget.value })}
                     />
                     <TextInput
                         label="Description"
                         value={currentCustomizableCategory.desc}
-                        onChange={(event)=>setCurrentCustomizableCategory({...currentCustomizableCategory, desc:event.currentTarget.value})}
+                        onChange={(event) => setCurrentCustomizableCategory({ ...currentCustomizableCategory, desc: event.currentTarget.value })}
                     />
                     <NumberInput
                         label="Required amount"
                         value={currentCustomizableCategory.required_amount}
-                        onChange={(val)=>setCurrentCustomizableCategory({...currentCustomizableCategory,required_amount:val})}
+                        onChange={(val) => setCurrentCustomizableCategory({ ...currentCustomizableCategory, required_amount: val })}
                     />
-                    <Button>Add to customizable category list</Button>
+                    <Button onClick={createCustomizableCategory}>Add to customizable category list</Button>
                 </div>
                 <div className="item">
                     <Text>ITEM</Text>
                     <TextInput
                         label="Name"
                         value={currentItem.name}
-                        onChange={(event)=>setCurrentItem({...currentItem, name:event.currentTarget.value})}
+                        onChange={(event) => setCurrentItem({ ...currentItem, name: event.currentTarget.value })}
                     />
                     <NumberInput
                         label="Price"
                         value={currentItem.name}
-                        onChange={(val)=>setCurrentItem({...currentItem,price:val})}
+                        onChange={(val) => setCurrentItem({ ...currentItem, price: val })}
                     />
                     <NumberInput
                         label="Servings"
                         value={currentItem.servings}
-                        onChange={(val)=>setCurrentItem({...currentItem, servings:val})}
+                        onChange={(val) => setCurrentItem({ ...currentItem, servings: val })}
                     />
                     <TextInput
                         label="Tags"
                         value={currentItem.tags}
-                        onChange={(event)=>setCurrentItem({...currentItem, tags:event.currentTarget.value})}
+                        onChange={(event) => setCurrentItem({ ...currentItem, tags: event.currentTarget.value })}
                     />
                     <TextInput
                         label="Ingredients"
                         value={currentItem.ingredients}
-                        onChange={(event)=>setCurrentItem({...currentItem, ingredients:event.currentTarget.value})}
+                        onChange={(event) => setCurrentItem({ ...currentItem, ingredients: event.currentTarget.value })}
                     />
                     <TextInput
                         label="Photo"
                         value={currentItem.photo}
-                        onChange={(event)=>setCurrentItem({...currentItem, photo:event.currentTarget.value})}
+                        onChange={(event) => setCurrentItem({ ...currentItem, photo: event.currentTarget.value })}
                     />
-                    <Button>Add to item list</Button>
+                    <Button onClick={createItem}>Add to item list</Button>
                 </div>
                 <div className="item">
                     <Text>SECTION</Text>
                     <TextInput
                         label="Name"
                         value={currentNewMenuSections}
-                        onChange={(event)=>setCurrentNewMenuSections(event.currentTarget.value)}
+                        onChange={(event) => setCurrentNewMenuSections(event.currentTarget.value)}
                     />
-                    <Button>Add to Section list</Button>
+                    <Button onClick={createMenuSection}>Add to Section list</Button>
                 </div>
             </form>
         </>
