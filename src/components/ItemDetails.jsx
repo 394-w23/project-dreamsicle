eeb5a2080cd6de638bd77883019cf71c89d43e3b
murirop <panashemuriro2023@u.northwestern.edu>
2023-03-02 10:13:01 -0600

    Added Checkboxes for custom items

--- src/components/ItemDetails.jsx
+++ src/components/ItemDetails.jsx
@@ -9,6 +9,9 @@ import {
   Group,
   Button,
   useMantineTheme,
+  Title,
+  Checkbox,
+  Radio,
 } from "@mantine/core";
 import QuantitySelector from "./QuantitySelector";
 import "./MenuItem.css";
@@ -70,6 +73,22 @@ const ItemDetails = ({
     setItemDetailsOpened(false);
   };
 
+
+  // handle addons
+
+  const handleAddOns = (selected, b) => {
+    console.log(selected, b)
+  }
+
+  const [limitedAddOns, setLimitedAddOns] = useState([]);
+
+  const handleAddOnsWithLimits = (selected, limit) => {
+    setLimitedAddOns(selected.splice(selected.length - limit, selected.length))
+    // TODO: handle pricing
+  }
+
+
+
   return (
     <Drawer
       opened={itemDetailsOpened}
@@ -85,8 +104,10 @@ const ItemDetails = ({
       overlayOpacity={0.55}
       overlayBlur={3}
       padding="lg"
+      root={{ backgroundColor: "red" }}
+
     >
-      <div className="menu-item">
+      <div className="menu-item" style={{ overflow: "scroll" }}>
         <Card shadow="sm" p="lg" radius="md" withBorder>
           <Card.Section>
             <Image src={itemDetails.photo} height={160} alt="Menu item image" />
@@ -98,12 +119,27 @@ const ItemDetails = ({
           </Group>
           {itemDetails["customizable-categories"] && (
             <Group position="apart" mt="md" mb="xs">
-              <Text weight={500}>Customization Options</Text>
+              <Title size="h3" >Customization Options</Title>
               {itemDetails["customizable-categories"].map((category) => {
+
+
                 return (
-                  <Group position="apart" mt="md" mb="xs">
-                    {category.name}
-                    {category["customizable-add-ons"].map((addOn) => <Text>{addOn.name}</Text>)}
+                  <Group style={{ flexDirection: "column", alignItems: "flex-start" }} position="apart" mt="md" mb="xs">
+                    <Title size="h4">{category.name}</Title>
+
+                    {/* Radio group or checkbox group */}
+                    {category["required-select-amount"] == 0 ?
+
+                      <Checkbox.Group style={{ flexDirection: "column", alignItems: "flex-start" }} onChange={(e) => { handleAddOns(e, category["customizable-add-ons"]) }}>
+                        {category["customizable-add-ons"].map((addOn, index) =>
+                          <Checkbox key={index} value={addOn.name} label={addOn.name} style={{ width: "100%" }} />)}
+                      </Checkbox.Group>
+                      :
+                      <Checkbox.Group style={{ flexDirection: "column", alignItems: "flex-start" }} value={limitedAddOns} onChange={(e) => { handleAddOnsWithLimits(e, category["required-select-amount"]) }}>
+                        {category["customizable-add-ons"].map((addOn, index) =>
+                          <Checkbox key={index} value={addOn.name} label={addOn.name} style={{ width: "100%" }} />)}
+                      </Checkbox.Group>
+                    }
                   </Group>
                 );
               })}
