7fd9a293e60ee015c16da331dbcc3e0d971cfb2a
finnwintz5 <102768648+finnwintz5@users.noreply.github.com>
2023-03-03 16:37:55 -0600

    Add ons added to cart and displayed

--- src/components/OrderTable.jsx
+++ src/components/OrderTable.jsx
@@ -1,9 +1,9 @@
 import { FaTrash } from "@react-icons/all-files/Fa/FaTrash"
 import { Button, Table } from "@mantine/core";
-import { menuItemParser } from '../utils/helper'
+import { menuItemParser, itemAddOnParser } from '../utils/helper'
+import "./OrderTable.css";
 
-
-export default function OrderTable({ restaurant, cartData, deletable,removeOrder }) {
+export default function OrderTable({ restaurant, cartData, deletable, removeOrder }) {
   let rows = []
   // console.log(cartData.orders, " =-------- ", restaurant)
 
@@ -11,21 +11,44 @@ export default function OrderTable({ restaurant, cartData, deletable,removeOrder
 
   let total_price = 0
   Object.values(cartData.orders).forEach(i => {
-    total_price += (i.quantity * (menuItemParser(i, restaurant).price))
+    let item = menuItemParser(i, restaurant);
+    let addOnList = itemAddOnParser(i, item);
+    let full_item_price=item.price;
+    for (let j=0;j<addOnList.length;j++) {
+      full_item_price+=addOnList[j].price;
+    }
+    total_price += (i.quantity * full_item_price)
+
   })
 
-  
+
 
 
   rows = Object.values(cartData.orders).map((order) => {
     let item = menuItemParser(order, restaurant);
+    let addOnList = itemAddOnParser(order, item);
+    console.log("addOnList", addOnList);
+    let full_item_price=item.price;
+    for (let i=0;i<addOnList.length;i++) {
+      full_item_price+=addOnList[i].price;
+    }
     return (
-      <tr key={order.id}>
-        <td>{item.name}</td>
-        <td>{order.quantity}</td>
-        <td>${(item.price * order.quantity).toFixed(2)}</td>
-        {deletable && <td><Button compact variant="subtle" onClick={() => removeOrder(order)}><FaTrash /></Button></td>}
-      </tr>
+      <>
+        <tr key={order.id}>
+          <td>{item.name}</td>
+          <td>{order.quantity}</td>
+          <td>${(full_item_price * order.quantity).toFixed(2)}</td>
+          {deletable && <td><Button compact variant="subtle" onClick={() => removeOrder(order)}><FaTrash /></Button></td>}
+        </tr>
+        {addOnList.map((addOn) => { return(
+          <div className="addOn">
+            {addOn.name}{addOn.price ? ` (+$${addOn.price})` : ""}
+          </div>)
+        })}
+
+
+      </>
+
     )
   });
   rows.push(<tr key={0}>
@@ -39,7 +62,7 @@ export default function OrderTable({ restaurant, cartData, deletable,removeOrder
       <thead>
         <tr>
           <th>Item</th>
-          <th>Mouths to feed</th>
+          <th>Quantity</th>
           <th>Price</th>
           {deletable && <th></th>}
         </tr>
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,24 +1,44 @@
 import { FaTrash } from "@react-icons/all-files/Fa/FaTrash"
-import { Button,  Table } from "@mantine/core";
+import { Button, Table } from "@mantine/core";
 
-export const tags = ["vegetarian","gluten-free","spicy","szechuan-cuisine","chinese-food"]
+export const tags = ["vegetarian", "gluten-free", "spicy", "szechuan-cuisine", "chinese-food"]
 
 export const menuItemParser = (order, restaurant) => {
-    let menuSections = restaurant.menu_sections;
-    let item;
-    try {
-      for (let menuSectionIndex = 0; menuSectionIndex < menuSections.length; menuSectionIndex++) {
-        let menuSectionItems = menuSections[menuSectionIndex].items;
-        for (let itemIndex = 0; itemIndex < menuSectionItems.length; itemIndex++) {
-          if (menuSectionItems[itemIndex].id == order.item) {
-            item = menuSectionItems[itemIndex];
-            break;
-          }
+  let menuSections = restaurant.menu_sections;
+  let item;
+  try {
+    for (let menuSectionIndex = 0; menuSectionIndex < menuSections.length; menuSectionIndex++) {
+      let menuSectionItems = menuSections[menuSectionIndex].items;
+      for (let itemIndex = 0; itemIndex < menuSectionItems.length; itemIndex++) {
+        if (menuSectionItems[itemIndex].id == order.item) {
+          item = menuSectionItems[itemIndex];
+          break;
         }
-        // break
       }
-    } catch (e) {
-      console.log(e);
+      // break
     }
-    return item;
-  };
+  } catch (e) {
+    console.log(e);
+  }
+  return item;
+};
+
+export const itemAddOnParser = (order, item) => {
+  let customizableCategories = item["customizable-categories"];
+  let addOnsIds=order.add_ons;
+  let addOnsOutput=[];
+  try {
+    for (let categoryIndex = 0; categoryIndex < customizableCategories.length; categoryIndex++) {
+      let addOns = customizableCategories[categoryIndex]["customizable-add-ons"];
+      for (let addOnIndex = 0; addOnIndex < addOns.length; addOnIndex++) {
+        if (addOnsIds.includes(addOns[addOnIndex].id)) {
+          addOnsOutput.push(addOns[addOnIndex]);
+        }
+      }
+      // break
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  return addOnsOutput;
+};
