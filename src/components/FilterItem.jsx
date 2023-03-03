e811c44b6884914a28bc3be1ced852e4577eef90
Weff24 <jwny123@gmail.com>
2023-03-03 16:00:07 -0600

    Changed filter address to zip code and reorganized restaurant list main filters

--- src/components/FilterItem.jsx
+++ src/components/FilterItem.jsx
@@ -4,7 +4,7 @@ import { TiDelete } from "@react-icons/all-files/ti/TiDelete";
 import { Badge } from '@mantine/core';
 
 const FilterItem = ({ tag, removeFilterTag }) => {
-
+  
   return (
     // <div onClick={() => setTagFilter(tag)} className="filter-tag">
     <div onClick={() => removeFilterTag(tag)} className="filter-tag">
