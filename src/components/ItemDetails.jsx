1f5a9dfe45ffede0fbf654baa50502dcb63c94c1
murirop <panashemuriro2023@u.northwestern.edu>
2023-03-04 13:26:53 -0600

    Cart can now validate add on selctions

--- src/components/ItemDetails.jsx
+++ src/components/ItemDetails.jsx
@@ -1,5 +1,5 @@
 import "./ItemDetails.css"
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   Card,
   Image,
@@ -31,22 +31,39 @@ const ItemDetails = ({
   const id = 0; ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////TODO: Hardcoded user ID
   const [quantity, setQuantity] = useState(0);
   const [raiseAlert, setRaiseAlert] = useState(false);
+  const errorsRef = useRef(null)
 
   const addToCart = () => {
     const new_uuid = uuid();
-    // console.log("quantity before new_order",quantity)
-
-
     let addOnsList = [];
-    Object.values(selectedAddOns).map(array=> {
-      array.map(addOn=> {
+    let categoryKeys = Object.keys(combinedAddOns)
+    let initialErrors = { ...initialAddOnErrors }
+    let addOnErrors = {}
+
+    categoryKeys.map(cKey => {
+      let cValue = combinedAddOns[cKey];
+      if (cValue.valid) {
+        delete initialErrors[cKey] // delete from errors because it's not an error now
+      }
+      if (Object.values(initialErrors).length > 0) {
+        addOnErrors = initialErrors;
+        setFinalAddOnErrors(addOnErrors);
+      }else{
+        addOnErrors = {}
+      }
+      cValue.ids.map(addOn => {
         addOnsList.push(JSON.parse(addOn).id)
       })
     })
-    // console.log(addOnsList);
+
+    if (Object.values(addOnErrors).length > 0) {
+      errorsRef.current.scrollIntoView({ behavior: 'smooth' });
+      return
+    }
 
     if (quantity === 0) {
       setRaiseAlert(true);
+      errorsRef.current.scrollIntoView({ behavior: 'smooth' });
     } else {
       const new_order = {
         id: new_uuid,
@@ -78,47 +95,70 @@ const ItemDetails = ({
     }
   };
   const closeDrawer = () => {
+    setCombinedAddOns({})
     setQuantity(0);
     setItemDetailsOpened(false);
-    setSelectedAddOns({})
+   
   };
 
   // Handle AddOns
-  const [selectedAddOns, setSelectedAddOns] = useState({});
   const [addOnTotalPrice, setAddOnTotalPrice] = useState(0);
+  const [combinedAddOns, setCombinedAddOns] = useState({});
+  const [initialAddOnErrors, setInitialAddOnErrors] = useState({}); // generated on open cart
+  const [finalAddOnErrors, setFinalAddOnErrors] = useState({}); // generated on final submission
 
+
+  // initialize required amount errors to use when validating selctions on add to cart
+  useEffect(() => {
+    if (itemDetails && itemDetails["customizable-categories"]) {
+      let c_categories = itemDetails["customizable-categories"];
+      let errors = {}
+      c_categories.map(cat => {
+        if (cat["required-select-amount"]) {
+          errors[cat.name] = `${cat.name} requires ${cat["required-select-amount"]} items to be selected`
+        }
+      })
+      setInitialAddOnErrors(errors)
+    }
+    setCombinedAddOns({})
+  }, [itemDetails])
 
   const handleSelectedAddOnsFormat = (selected, limit, category) => {
-    let addOns = selectedAddOns[category] || [];
+    let addOns = combinedAddOns[category] || [];
     if (limit == 0) {
       addOns = selected;
     } else {
       addOns = selected.splice(selected.length - limit, selected.length)
     }
 
-    let rawLimitedAddOns = { ...selectedAddOns };
-    rawLimitedAddOns[category] = addOns;
-    setSelectedAddOns(rawLimitedAddOns);
+    let rawAddOns = { ...combinedAddOns };
+    let rawAddOnsCategory = rawAddOns[category] || {}
+    rawAddOnsCategory.ids = addOns;
+    rawAddOnsCategory.required_amount = limit;
+    rawAddOnsCategory.valid = limit == 0 || addOns.length == limit
+    rawAddOns[category] = rawAddOnsCategory
+    setCombinedAddOns(rawAddOns);
   }
 
   // helper to calculate the total of the add on prices
   const calculateAddOnPrices = (selectedAddOns) => {
     let total = 0;
     let arrays = Object.values(selectedAddOns)
-    arrays.map(array=> {
-      array.map(x=> {
-
+    arrays.map(array => {
+      array.ids.map(x => {
         let price = Number(JSON.parse(x).price);
         total += price;
       })
     })
     return total;
+
   }
 
   // handle price total for add on selections
   useEffect(() => {
-    setAddOnTotalPrice(calculateAddOnPrices(selectedAddOns));
-  }, [selectedAddOns])
+    setAddOnTotalPrice(calculateAddOnPrices(combinedAddOns));
+  }, [combinedAddOns])
+
   let placeholderImage = "https://theme-assets.getbento.com/sensei/f9c493b.sensei/assets/images/catering-item-placeholder-704x520.png";
 
   return (
@@ -136,13 +176,13 @@ const ItemDetails = ({
       overlayOpacity={0.55}
       overlayBlur={3}
       padding="lg"
-   
+
 
     >
       <div className="menu-item" >
         <Card shadow="sm" p="lg" radius="md" withBorder >
           <Card.Section>
-            <Image src={itemDetails.photo !== "n/a"? itemDetails.photo : placeholderImage} height={160} alt="Menu item image" />
+            <Image src={itemDetails.photo !== "n/a" ? itemDetails.photo : placeholderImage} height={160} alt="Menu item image" />
           </Card.Section>
 
           <Group position="apart" mt="md" mb="xs">
@@ -151,16 +191,16 @@ const ItemDetails = ({
           </Group>
           {itemDetails["customizable-categories"] && (
 
-               <Group  style={{ flexDirection: "column", alignItems: "flex-start" }} position="apart" mt="md" mb="xs">
+            <Group style={{ flexDirection: "column", alignItems: "flex-start" }} position="apart" mt="md" mb="xs">
               <Title size="h3" >Customization Options</Title>
               {itemDetails["customizable-categories"].map((category, index) => {
                 return (
                   <Group key={index} style={{ flexDirection: "column", alignItems: "flex-start" }} position="apart" mt="md" mb="xs">
                     <Title size="h4">{category.name}</Title>
 
-                    <Checkbox.Group style={{ flexDirection: "column", alignItems: "flex-start" }} value={selectedAddOns[category.name]} onChange={(e) => { handleSelectedAddOnsFormat(e, category["required-select-amount"], category.name) }}>
+                    <Checkbox.Group style={{ flexDirection: "column", alignItems: "flex-start" }} value={combinedAddOns[category.name] && combinedAddOns[category.name].ids} onChange={(e) => { handleSelectedAddOnsFormat(e, category["required-select-amount"], category.name) }}>
                       {category["customizable-add-ons"].map((addOn, index) =>
-                        <Checkbox key={index} value={JSON.stringify(addOn)} label={ addOn.price ? `${addOn.name} (+$${addOn.price})`: addOn.name} style={{ width: "100%" }} />)}
+                        <Checkbox key={index} value={JSON.stringify(addOn)} label={addOn.price ? `${addOn.name} (+$${addOn.price})` : addOn.name} style={{ width: "100%" }} />)}
                     </Checkbox.Group>
 
                   </Group>
@@ -172,20 +212,17 @@ const ItemDetails = ({
 
           <Group position="apart" mt="md" mb="xs">
             <Text>{itemDetails.servings} servings</Text>
-            <QuantitySelector setQuantity={setQuantity} quantity={quantity} minAllowedAmount={0}/>
+            <QuantitySelector setQuantity={setQuantity} quantity={quantity} minAllowedAmount={0} />
           </Group>
           <Text position="right">
-            Subtotal: ${(isNaN(quantity)?  "0.00": ((quantity * itemDetails.price) + (quantity * addOnTotalPrice)).toFixed(2))} 
+            Subtotal: ${(isNaN(quantity) ? "0.00" : ((quantity * itemDetails.price) + (quantity * addOnTotalPrice)).toFixed(2))}
           </Text>
-        </Card>
-        <div style={{ textAlign: "center", marginTop: 20, marginBottom: 100 }}>
-        </div>
-        <div className="submit-button" >
-          <Button onClick={addToCart}>
-            Add To Cart
-          </Button>
-        </div>
-        {raiseAlert && (
+
+          <div style={{ textAlign: "center", marginTop: 20, marginBottom: 100, }} >
+            {Object.values(finalAddOnErrors).map((error, index) => <Alert key={index} style={{marginBottom: 10}} title="There was a problem" color="red">
+              {error}
+            </Alert>)}
+            {raiseAlert && (
           <Alert
             icon={<BiErrorCircle size={16} />}
             title="Minimum Order"
@@ -194,6 +231,19 @@ const ItemDetails = ({
             You must order at least 1 to add to cart!
           </Alert>
         )}
+          </div>
+         
+
+          <div ref={errorsRef}></div>
+
+        </Card>
+
+        <div className="submit-button" >
+          <Button onClick={addToCart}>
+            Add To Cart
+          </Button>
+        </div>
+        
       </div>
     </Drawer>
   );
