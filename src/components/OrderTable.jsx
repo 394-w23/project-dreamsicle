902d8e7a9b5dc9bbb79525d70337bace9c27cae1
finnwintz5 <102768648+finnwintz5@users.noreply.github.com>
2023-03-05 14:01:45 -0600

    Simple styling fix to the table

--- src/components/OrderTable.jsx
+++ src/components/OrderTable.jsx
@@ -1,6 +1,6 @@
 import { FaTrash } from "@react-icons/all-files/Fa/FaTrash"
 import { Button, Table } from "@mantine/core";
-import { menuItemParser, itemAddOnParser,getTotalOrderPrice } from '../utils/helper'
+import { menuItemParser, itemAddOnParser, getTotalOrderPrice } from '../utils/helper'
 import "./OrderTable.css";
 
 export default function OrderTable({ restaurant, cartData, deletable, removeOrder }) {
@@ -9,7 +9,7 @@ export default function OrderTable({ restaurant, cartData, deletable, removeOrde
 
 
 
-  let total_price = getTotalOrderPrice(restaurant,cartData);
+  let total_price = getTotalOrderPrice(restaurant, cartData);
   console.log(total_price)
   // Object.values(cartData.orders).forEach(i => {
   //   let item = menuItemParser(i, restaurant);
@@ -29,9 +29,9 @@ export default function OrderTable({ restaurant, cartData, deletable, removeOrde
     let item = menuItemParser(order, restaurant);
     let addOnList = itemAddOnParser(order, item);
     // console.log("addOnList", addOnList);
-    let full_item_price=item.price;
-    for (let i=0;i<addOnList.length;i++) {
-      full_item_price+=addOnList[i].price;
+    let full_item_price = item.price;
+    for (let i = 0; i < addOnList.length; i++) {
+      full_item_price += addOnList[i].price;
     }
     return (
       <>
@@ -41,10 +41,11 @@ export default function OrderTable({ restaurant, cartData, deletable, removeOrde
           <td>${(full_item_price * order.quantity).toFixed(2)}</td>
           {deletable && <td><Button compact variant="subtle" onClick={() => removeOrder(order)}><FaTrash /></Button></td>}
         </tr>
-        {addOnList.map((addOn) => { return(
-          <div className="addOn">
-            {addOn.name}{addOn.price ? ` (+$${addOn.price})` : ""}
-          </div>)
+        {addOnList.map((addOn) => {
+          return (
+            <div className="addOn">
+              {addOn.name}{addOn.price ? ` (+$${addOn.price})` : ""}
+            </div>)
         })}
 
 
